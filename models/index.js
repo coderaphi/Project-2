5f0bafb9f652dc222c9587866b5b5dfc89e753ed
Rasan <rashmedo@gmail.com>
2019-03-06 23:18:26 -0500

    procfile

--- models/index.js
+++ models/index.js
@@ -10,7 +10,10 @@ var dog_fixture = require('../data/dog_fixture');
 var db = {};
 
 if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.use_env_variable]);
+  var sequelize = new Sequelize(
+    process.env[config.use_env_variable],
+    { dialect: config.dialect }
+  );
 } else {
   var sequelize = new Sequelize(
     config.database,
